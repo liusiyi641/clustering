a47ad5135834441b3a8df4e6a5d912e9249d6401
Liusiyi <liusiyi@bu.edu>
2019-09-30 22:59:04 -0400

    extra credit hw done

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -2,6 +2,11 @@ from collections import defaultdict
 from math import inf
 import random
 import csv
+import math
+
+#import numpy as np
+#import random
+#import pandas as np
 
 
 def point_avg(points):
@@ -11,7 +16,22 @@ def point_avg(points):
     
     Returns a new point which is the center of all the points.
     """
-    raise NotImplementedError()
+    assert len(points)>0
+    
+    res = [0]*len(points[0])
+    
+    for point in points:
+        for ind, entry in enumerate(point):
+            #print(type(entry))
+            #print(res[ind])
+            res[ind] += int(entry)
+
+    res = [tmp/len(points) for tmp in res]
+    
+    
+    return res
+
+#print(point_avg([[1,2,3,4],[5,6,7,8]]))
 
 
 def update_centers(data_set, assignments):
@@ -21,7 +41,20 @@ def update_centers(data_set, assignments):
     Compute the center for each of the assigned groups.
     Return `k` centers in a list
     """
-    raise NotImplementedError()
+
+    dic = {}
+    for ind, point in enumerate(data_set):
+        if assignments[ind] not in dic:
+            dic[assignments[ind]] = [point]
+        else:
+            dic[assignments[ind]].append(point)
+
+    res = []
+    for i in list(dic.keys()):
+        res.append(point_avg(dic[i]))
+    
+    
+    return res
 
 
 def assign_points(data_points, centers):
@@ -44,23 +77,49 @@ def distance(a, b):
     """
     Returns the Euclidean distance between a and b
     """
-    raise NotImplementedError()
+    
+    #return np.linalg.norm(np.array(a)-np.array(b))
+    #print(a)
 
+    return math.sqrt(sum([(int(x) - int(y)) ** 2 for x, y in zip(a, b)]))
+
+
+#print(distance([2,3,4],[4,5,6]))
 
 def generate_k(data_set, k):
     """
     Given `data_set`, which is an array of arrays,
     return a random set of k points from the data_set
     """
-    raise NotImplementedError()
+    
+    return [data_set[i] for i in random.sample(range(len(data_set)),k)]
 
-
+print(generate_k([[2,3,4],[4,5,6],[7,8,9]],2))
+    
 def get_list_from_dataset_file(dataset_file):
-    raise NotImplementedError()
+
+    with open(dataset_file, 'r') as file:
+        dataset = csv.reader(file)
+        res = []
+        for point in dataset:
+            res.append(point)
 
 
+
+    return res 
+
+
+
+
+    #return pd.read_csv(dataset_file).values.toList()
+    
 def cost_function(clustering):
-    raise NotImplementedError()
+    cost = 0
+    for cluster in clustering.keys():
+        center = point_avg(clustering[cluster])
+        for point in clustering[cluster]:
+            cost += distance(point, center)
+    return cost
 
 
 def k_means(dataset_file, k):
